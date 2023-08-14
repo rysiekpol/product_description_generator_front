7cc70e4e9debfae324a6c0b036af7340550b0153
Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>
2023-08-14 12:27:11 +0200

    Refactor even more, create hook

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,107 +1,56 @@
-import React, { useEffect, useState, useContext, useCallback } from 'react';
+import React, { useContext } from 'react';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import TokenContext from '../context/TokenContext'; 
 import UpdateProduct from '../components/form/UpdateProductForm';
 import ShareForm from '../components/form/ShareForm';
 import TranslationForm from '../components/form/TranslationForm';
-import { fetchProducts } from '../services/fetchProducts';
 import ProductList from '../components/ui/ProductList';
 import { handleAddDescription } from '../utils/descriptionUtils';
-import { shareProduct } from '../services/shareProduct';
-import { showToast } from '../utils/toastUtils';
-import { translateText } from '../services/translateText';
+import { useProductOperations } from '../hooks/useProductOperations';
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [nextPage, setNextPage] = useState(null);
-  const [prevPage, setPrevPage] = useState(null);
-  const { isTokenChecked } = useContext(TokenContext);
-  const [showUpdateForm, setShowUpdateForm] = useState(false);
-  const [currentProduct, setCurrentProduct] = useState(null);
-  const [currentDescription, setCurrentDescription] = useState(null);
-  const [showShareForm, setShowShareForm] = useState(false);
-  const [showTranslationForm, setShowTranslationForm] = useState(false);
+    const { isTokenChecked } = useContext(TokenContext);
+    const productOps = useProductOperations(1, isTokenChecked);
 
-
-  const fetchData = useCallback(async () => {
-    try {
-        const data = await fetchProducts(currentPage);
-        setProducts(data.results);
-        setNextPage(data.next);
-        setPrevPage(data.previous);
-    } catch (error) {
-        console.error(error);
-    }
-}, [currentPage]);
-
-  const handleShare = async (data, e) => {
-    const result = await shareProduct(data.productid, data.email, data.sharetime);
-    if (result.success) {
-      showToast('Product shared', 'success');
-      console.log('Product shared:', result.data);
-    } else {
-      console.error('Failed to share product:', result.error);
-    }
-    setShowShareForm(false);
-    setCurrentProduct(null);
-  };
-
-  const handleTranslate = async (data, e) => {
-    const result = await translateText(data.descriptionid, data.languages);
-    if (result.success) {
-      showToast('Translations will be sent to your email!', 'success');
-    } else {
-      console.error('Failed to translate text:', result.error);
-    }
-    setShowTranslationForm(false);
-    setCurrentDescription(null);
-  };
-
-  useEffect(() => {
-    if (!isTokenChecked) return;
-    fetchData();
-  }, [currentPage, isTokenChecked, fetchData]);
-
-  return (
-    <div>
-      {showUpdateForm ? (
-        <UpdateProduct
-          product={currentProduct} 
-          onClose={() => {
-            setShowUpdateForm(false);
-            setCurrentProduct(null);
-          }} 
-          refreshProducts={fetchData}
-        />
-      ) : showShareForm ? (
-        <ShareForm 
-          productId={currentProduct.id}
-          onSubmit={handleShare}
-        />
-      ) : showTranslationForm ? (
-        <TranslationForm 
-          descriptionId={currentDescription.id}
-          onSubmit={handleTranslate}
-        />
-      ) : (
-        <ProductList 
-            products={products}
-            handleAddDescription={handleAddDescription}
-            setCurrentProduct={setCurrentProduct}
-            setCurrentDescription={setCurrentDescription}
-            setShowUpdateForm={setShowUpdateForm}
-            setShowShareForm={setShowShareForm}
-            setShowTranslationForm={setShowTranslationForm}
-            prevPage={prevPage}
-            nextPage={nextPage}
-            setCurrentPage={setCurrentPage}
-            fetchData={fetchData}
-        />
-      )}
-    </div>
-  );
+    return (
+        <div>
+            {productOps.showUpdateForm ? (
+                <UpdateProduct
+                    product={productOps.currentProduct}
+                    onClose={() => {
+                        productOps.setShowUpdateForm(false);
+                        productOps.setCurrentProduct(null);
+                    }}
+                    refreshProducts={productOps.fetchData}
+                />
+            ) : productOps.showShareForm ? (
+                <ShareForm 
+                    productId={productOps.currentProduct.id}
+                    onSubmit={productOps.handleShare}
+                />
+            ) : productOps.showTranslationForm ? (
+                <TranslationForm 
+                    descriptionId={productOps.currentDescription.id}
+                    onSubmit={productOps.handleTranslate}
+                />
+            ) : (
+                <ProductList 
+                    products={productOps.products}
+                    handleAddDescription={handleAddDescription}
+                    setCurrentProduct={productOps.setCurrentProduct}
+                    setCurrentDescription={productOps.setCurrentDescription}
+                    setShowUpdateForm={productOps.setShowUpdateForm}
+                    setShowShareForm={productOps.setShowShareForm}
+                    setShowTranslationForm={productOps.setShowTranslationForm}
+                    prevPage={productOps.prevPage}
+                    nextPage={productOps.nextPage}
+                    setCurrentPage={productOps.setCurrentPage}
+                    fetchData={productOps.fetchData}
+                />
+            )}
+        </div>
+    );
 };
 
-export default Products; 
+export default Products;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import SearchBar from '../components/ui/SearchBar';
-import { fetchProducts  } from '../services/searchProducts';
-import { useDebounce } from 'usehooks-ts'
+import { useDebounce } from 'usehooks-ts';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import UpdateProduct from '../components/form/UpdateProductForm';
@@ -9,106 +8,55 @@ import ShareForm from '../components/form/ShareForm';
 import TranslationForm from '../components/form/TranslationForm';
 import ProductList from '../components/ui/ProductList';
 import { handleAddDescription } from '../utils/descriptionUtils';
-import { shareProduct } from '../services/shareProduct';
-import { showToast } from '../utils/toastUtils';
-import { translateText } from '../services/translateText';
-
+import { useProductOperations } from '../hooks/useProductOperations';
 
 const Search = () => {
-  const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [nextPage, setNextPage] = useState(null);
-  const [prevPage, setPrevPage] = useState(null);
-  const [showUpdateForm, setShowUpdateForm] = useState(false);
-  const [currentProduct, setCurrentProduct] = useState(null);
-  const [currentDescription, setCurrentDescription] = useState(null);
-  const [showShareForm, setShowShareForm] = useState(false);
-  const [showTranslationForm, setShowTranslationForm] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
+    const debouncedValue = useDebounce(searchTerm, 500);
 
-  const debouncedValue = useDebounce(searchTerm, 500)
+    const productOps = useProductOperations(1, true, debouncedValue);
 
-  const fetchData = useCallback(async () => {
-    if (debouncedValue !== ""){
-    try {
-        const data = await fetchProducts(debouncedValue, currentPage);
-        setProducts(data.results);
-        setNextPage(data.next);
-        setPrevPage(data.previous);
-    } catch (error) {
-        console.error(error);
-    }
-  }
-  }, [debouncedValue, currentPage]);
-
-  const handleShare = async (data, e) => {
-    const result = await shareProduct(data.productid, data.email, data.sharetime);
-    if (result.success) {
-      showToast('Product shared', 'success');
-      console.log('Product shared:', result.data);
-    } else {
-      console.error('Failed to share product:', result.error);
-    }
-    setShowShareForm(false);
-    setCurrentProduct(null);
-  };
-
-  const handleTranslate = async (data, e) => {
-    const result = await translateText(data.descriptionid, data.languages);
-    if (result.success) {
-      showToast('Translations will be sent to your email!', 'success');
-    } else {
-      console.error('Failed to translate text:', result.error);
-    }
-    setShowTranslationForm(false);
-    setCurrentDescription(null);
-  };
-
-  useEffect(() => {
-      fetchData();
-  }, [fetchData]);
-
-  return (
-    <div>
-      {showUpdateForm ? (
-        <UpdateProduct
-          product={currentProduct} 
-          onClose={() => {
-            setShowUpdateForm(false);
-            setCurrentProduct(null);
-          }} 
-          refreshProducts={fetchData}
-        />
-      ) : showShareForm ? (
-        <ShareForm 
-          productId={currentProduct.id}
-          onSubmit={handleShare}
-        />
-      ) : showTranslationForm ? (
-        <TranslationForm 
-          descriptionId={currentDescription.id}
-          onSubmit={handleTranslate}
-        />
-      ) : (
-        <>
-          <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-          <ProductList 
-            products={products}
-            handleAddDescription={handleAddDescription}
-            setCurrentProduct={setCurrentProduct}
-            setCurrentDescription={setCurrentDescription}
-            setShowUpdateForm={setShowUpdateForm}
-            setShowShareForm={setShowShareForm}
-            setShowTranslationForm={setShowTranslationForm}
-            prevPage={prevPage}
-            nextPage={nextPage}
-            setCurrentPage={setCurrentPage}
-            fetchData={fetchData}
-        />
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            {productOps.showUpdateForm ? (
+                <UpdateProduct
+                    product={productOps.currentProduct}
+                    onClose={() => {
+                        productOps.setShowUpdateForm(false);
+                        productOps.setCurrentProduct(null);
+                    }}
+                    refreshProducts={productOps.fetchData}
+                />
+            ) : productOps.showShareForm ? (
+                <ShareForm 
+                    productId={productOps.currentProduct.id}
+                    onSubmit={productOps.handleShare}
+                />
+            ) : productOps.showTranslationForm ? (
+                <TranslationForm 
+                    descriptionId={productOps.currentDescription.id}
+                    onSubmit={productOps.handleTranslate}
+                />
+            ) : (
+                <>
+                    <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                    <ProductList 
+                        products={productOps.products}
+                        handleAddDescription={handleAddDescription}
+                        setCurrentProduct={productOps.setCurrentProduct}
+                        setCurrentDescription={productOps.setCurrentDescription}
+                        setShowUpdateForm={productOps.setShowUpdateForm}
+                        setShowShareForm={productOps.setShowShareForm}
+                        setShowTranslationForm={productOps.setShowTranslationForm}
+                        prevPage={productOps.prevPage}
+                        nextPage={productOps.nextPage}
+                        setCurrentPage={productOps.setCurrentPage}
+                        fetchData={productOps.fetchData}
+                    />
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Search;
